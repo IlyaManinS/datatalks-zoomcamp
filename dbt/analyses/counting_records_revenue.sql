71b003a22295eb6aafb8a7aab1c98941aa723217
Ilya Manin <ilyamanin1993@gmail.com>
2026-02-17 00:01:06 +0000

    Adding analyses for the homework

---
+++ dbt/analyses/counting_records_revenue.sql
@@ -0,0 +1,4 @@
+SELECT COUNT(*)
+FROM {{ref('monthly_revenue_per_location')}}
+
+-- answer: 12184
---
+++ dbt/analyses/green_taxi_trip_counts.sql
@@ -0,0 +1,5 @@
+SELECT SUM(total_monthly_trips) as sum_total_monthly_trips
+FROM {{ref('monthly_revenue_per_location')}}
+WHERE revenue_month = '2019-10-01'
+AND service_type = 'Green'
+
